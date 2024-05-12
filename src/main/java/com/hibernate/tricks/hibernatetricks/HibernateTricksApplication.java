f712ec073482aaa5ddff9f23ef80cf10c9a4839c
med-chhoub <chhoub.mohamed@gmail.com>
2024-05-12 16:35:08 +0200

    first commit

---
+++ src/main/java/com/hibernate/tricks/hibernatetricks/HibernateTricksApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate.tricks.hibernatetricks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateTricksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateTricksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hibernate/tricks/hibernatetricks/HibernateTricksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate.tricks.hibernatetricks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateTricksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
